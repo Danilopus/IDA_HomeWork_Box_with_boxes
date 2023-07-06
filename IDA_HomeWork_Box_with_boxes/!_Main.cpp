8f8a4f9738e998cdf71e7addacf7cbfc87e8015e
Ишутинов Данил <gladius@inbox.ru>
2023-07-07 01:03:46 +0500

    Add project files.

---
+++ IDA_HomeWork_Box_with_boxes/!_Main.cpp
@@ -0,0 +1,78 @@
+﻿// HomeWork template project v 6.0 // 27.06.2023 //
+
+/*
+
+Задание 1
+
+*/
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <time.h>
+#include <algorithm>
+#include <conio.h>
+#include <cassert>
+//My own headers
+#include "Service functions.h"
+#include "MainMenu.h"
+#include "Box.h"
+
+
+
+// Homework OOP 08 ----------------------------------------------------	
+
+
+//Task 1 Overcoat class demo with overloaded operators: = | == | < | > 
+void Task_1(std::string name_of_task)
+{
+//	std::cout <<
+		Box box_1(50,50,50);
+		Box box_2(25, 25, 25);
+		Box box_3(10, 10, 10);
+		Box box_4(5, 5, 5);
+		Box box_5(2, 2, 2);
+
+		box_1.pushback(box_2).pushback(box_3);
+		box_3.pushfront(box_5).pushfront(box_4);
+	
+		//assert((box_1.size() != 4) & "box_1.size()!=4");
+		std::cout << box_1.size();
+		std::cout << box_3.size();
+
+
+	_getch();
+}
+
+//Task 2
+void Task_2(std::string name_of_task)
+{
+}
+
+//Task 3 
+void Task_3() {}
+
+//Task 4
+void Task_4() {}
+
+// MAIN ------- MAIN ------- MAIN ------- MAIN ------- MAIN --------
+int main()
+{
+	srand(time(NULL));
+	//setlocale(LC_CTYPE, "Russian");
+	//system("mode con cols=60 lines=40"); 
+	Main_menu MainMenu;	
+	MainMenu.AddElement("OOP Home Work 07: ");	// Homework name
+	MainMenu.AddElement("Collection class demo");
+	//MainMenu.AddElement("Flat class demo with overloaded operators: = | == | < | > ");
+
+	//MainMenu.AddElement("XXX");
+
+	do 
+	{		
+		MainMenu.Show_menu();
+		if (!MainMenu.User_Choice_Handle(_getch()))	break;
+	} while (true);
+	return 0;
+}
+
---
+++ IDA_HomeWork_Box_with_boxes/Box.h
@@ -0,0 +1,177 @@
+﻿#pragma once
+#include <vector>
+
+//#include "Service functions.h"
+
+class Box
+{
+	struct BoxDimensions { int _width; int _height; int _depth; } _dimensions;
+	//int _inner_boxes = 0;
+	std::vector<Box*> _inner_boxes_list;
+public:
+	Box(int width, int height, int depth)
+	{
+		_dimensions._depth = depth;
+		_dimensions._height = height;
+		_dimensions._width = width;
+	}
+	Box& pushfront(Box& new_value)
+	{
+		if (_inner_boxes_list.size() == 0)
+		{
+			_inner_boxes_list.push_back(&new_value);
+			return *this;
+		}
+		for(int i=_inner_boxes_list.size(); i>0;--i)
+		{
+			_inner_boxes_list.push_back(_inner_boxes_list[_inner_boxes_list.size() - 1]); //сохраняем последний элемент в новую ячейку
+			_inner_boxes_list[i] = _inner_boxes_list[i - 1];
+		}
+		_inner_boxes_list[0] = &new_value;
+		return *this;
+	}
+	Box& pushback(Box& new_value)
+	{
+		_inner_boxes_list.push_back(&new_value);
+
+		return *this;
+	}
+	Box& popfront()
+	{
+		for (int i = 0; i<_inner_boxes_list.size(); --i)
+		{
+			_inner_boxes_list[i] = _inner_boxes_list[i + 1];
+		}		
+		_inner_boxes_list.pop_back();
+		return *this;
+	}
+	Box& popback()
+	{
+		_inner_boxes_list.pop_back();
+		return *this;
+	}
+	Box& insert(size_t pos, Box& new_value)
+	{
+		_inner_boxes_list.insert(_inner_boxes_list.begin()+pos, &new_value);
+		return *this;
+	}
+	Box& clear()
+	{
+		_inner_boxes_list.clear();
+		return *this;
+	}
+
+	size_t size() 
+	{ 
+		size_t inner_size = 0;
+		if (_inner_boxes_list.size())
+		{
+			for (int i = 0; i < _inner_boxes_list.size(); i++)
+				inner_size += _inner_boxes_list[i]->size();
+		}		
+		return 	(inner_size+_inner_boxes_list.size());
+	}
+
+	//Box* begin() { return _inner_boxes_list.begin(); }
+	//Box* end() { return _inner_boxes_list.end(); }
+
+	// Shows ---------------------------------------------------------------
+	//void ShowValues()
+	//{
+	//	//std::cout << this; 
+
+	//	if (_values_list == nullptr)
+	//	{
+	//		std::cout << "[no data]";
+	//		return;
+	//	}
+
+	//	std::cout << "[ ";
+	//	for (int i = 0; i < _size; i++)
+	//		//out << Box_obj->_values_list[i] << " | ";
+	//		std::cout << _values_list[i] << " | ";
+	//	std::cout << "\b\b]";
+
+	//}
+
+	//Overload operators - reference returned -----------------------------------------	
+
+	/*
+	Box& operator + (const Box& Box_obj) const
+
+	{
+		Box& new_container = *new Box;
+		for (size_t i = 0; i < _size; i++)
+			new_container.pushback(_values_list[i]);
+
+		for (size_t i = 0; i < Box_obj._size; i++)
+			new_container.pushback(Box_obj._values_list[i]);
+
+		return new_container;
+	}
+	*/
+	/*
+	//Box<any>& operator = (const Box<any>& Box_obj)
+	Box& operator = (const Box& Box_obj)
+	{
+		if (_size >= Box_obj._size)
+		{
+			for (size_t i = 0; i < Box_obj._size; i++)
+				_values_list[i] = Box_obj[i];
+			_size = Box_obj._size;
+			if (_size < _capacity / 2) Shrink_capacity();
+		}
+		else
+		{
+			any* new_list_for_copie = new any[Box_obj._capacity];
+			for (size_t i = 0; i < Box_obj._size; i++)
+				new_list_for_copie[i] = Box_obj[i];
+			delete[] _values_list;
+			_values_list = new_list_for_copie;
+			_size = Box_obj._size;
+			_capacity = Box_obj._capacity;
+		}
+		return *this;
+	}
+	*/
+
+	Box& operator [ ] (size_t index) { return *_inner_boxes_list[index]; }
+	//Box& operator [ ] (size_t index) const { return *(_inner_boxes_list.begin() + index); }
+
+
+	/*
+	friend std::ostream& operator << (std::ostream& out, Box* Box_obj)
+	{
+		if (Box_obj->_values_list == nullptr)
+			return out << "[no data]";
+
+		out << "[ ";
+		for (int i = 0; i < Box_obj->_size; i++)
+			//out << Box_obj->_values_list[i] << " | ";
+			out << (*Box_obj)[i] << " | ";
+		out << "\b\b]";
+		return out;
+	}
+	friend std::ostream& operator << (std::ostream& out, Box& Box_obj)
+	{
+		if (Box_obj._values_list == nullptr)
+			return out << "[no data]";
+
+		out << "[ ";
+		for (int i = 0; i < Box_obj._size; i++)
+			//		out << Box_obj._values_list[i] << " | ";
+			out << Box_obj[i] << " | ";
+		out << "\b\b]";
+		return out;
+	}
+	friend std::istream& operator >> (std::istream& in, Box& Box_obj)
+	{
+		//Box_obj.pushback(Get_LL_SA());
+		return in;
+	}
+	*/
+};
+
+
+
+
